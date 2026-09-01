92e2aa38353e0f6ca23bd3d64be38467a30027c5
Lahlan <106949397+lahlan-h@users.noreply.github.com>
2026-09-01 10:51:19 +1000

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const output = new Array(nums.length).fill(1);
+        let prefix = 1;
+        let postfix = 1;
+
+        // prefix traversal
+        for(let i = 0; i < nums.length; i++){
+            // multiply output.at(i) with prefix
+            output[i] = output.at(i) * prefix;
+
+            // multiply prefix with nums.at(i) 
+            prefix *= nums.at(i);
+        }   
+
+        // postfix traversal
+        for(let i = nums.length - 1; i >= 0; i--){
+             // multiply output.at(i) with postfix
+            output[i] = output.at(i) * postfix;
+
+            // multiply postfix with nums.at(i)
+            postfix *= nums.at(i);
+        }
+            
+        return output; 
+    }
+}
